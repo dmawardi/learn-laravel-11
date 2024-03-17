d9b70c4cde67791f4c00879aaaf02ccadff1de8b
dmawardi <dmawardi@gmail.com>
2024-03-17 16:00:13 +0800

    Nav link now conditionally outputs anchor or button tag based on prop value

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -1,9 +1,16 @@
-@props(['active' => false])
+@props(['active' => false, 'type' => 'a'])
 
 @php
     $classes = $active ?? false ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white';
 @endphp
 
 {{-- attributes inputs all attributes from input: ie. class, href, id, etc. --}}
-<a {{ $attributes }} class="{{ $classes }} rounded-md px-3 py-2 text-sm font-medium"
-    aria-current="{{ $active ? 'page' : 'false' }}">{{ $slot }}</a>
+@if ($type == 'a')
+    <a {{ $attributes }} class="{{ $classes }} block rounded-md px-3 py-2 text-base font-medium"
+        aria-current="{{ $active ? 'page' : 'false' }}">{{ $slot }}</a>
+@else
+    <button {{ $attributes }} class="{{ $classes }} block rounded-md px-3 py-2 text-base font-medium"
+        aria-current="{{ $active ? 'page' : 'false' }}">{{ $slot }}</button>
+@endif
+{{-- <a {{ $attributes }} class="{{ $classes }} rounded-md px-3 py-2 text-sm font-medium"
+    aria-current="{{ $active ? 'page' : 'false' }}">{{ $slot }}</a> --}}
