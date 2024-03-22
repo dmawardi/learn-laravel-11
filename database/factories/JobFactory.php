75c0976c4199da6879f33b526aa4a526c769f2a6
dmawardi <dmawardi@gmail.com>
2024-03-22 13:18:54 +0800

    Completed episode 8. Added migration file for job listings

---
+++ database/factories/JobFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Job>
+ */
+class JobFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->jobTitle(),
+            'salary' => strval(fake()->randomFloat(2, 50000, 100000)),
+        ];
+    }
+}
