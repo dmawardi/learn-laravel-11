2fb264be19f9c10bdb6fc3b00d724a4e9bb13b92
dmawardi <dmawardi@gmail.com>
2024-03-25 15:22:12 +0800

    Using Eloquent for Job model

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -7,27 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Support\Arr;
 
-class Job
+class Job extends Model
 {
+    // Use the protected $table property to specify the table name if it is different from the default convention
+    // ie. the plural form of the class name
+    protected $table = 'job_listings';
+
+    protected $fillable = ['title', 'salary'];
+
     use HasFactory;
 
-    public static function all(): array {
-        return [
-            ['title' => 'PHP Developer', 'salary' => '$50,000', 'id' => 1],
-            ['title' => 'Python Developer', 'salary' => '$60,000', 'id' => 2],
-            ['title' => 'Ruby Developer', 'salary' => '$70,000', 'id' => 3],
-            ['title' => 'Java Developer', 'salary' => '$80,000', 'id' => 4],
-            ['title' => 'DevOps', 'salary' => '$90,000', 'id' => 5],
-            ['title' => 'Node.js Developer', 'salary' => '$100,000', 'id' => 6]
-        ];
-    }
-    public static function find(int $id): array  {
-        // Use the array helper class to find the first job with the given id
-        $job = Arr::first(static::all(), fn($job) => $job['id'] == $id);
-        if (!$job) {
-            return abort(404, "Job not found");
-        } else {
-            return $job;
-        }
-    }
 }
