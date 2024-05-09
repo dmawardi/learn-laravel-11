9f02cb4c1945e1f00b97da089256dd9e6b15473e
dmawardi <dmawardi@gmail.com>
2024-05-09 16:45:14 +0800

    Added Tailwind using Vite bundler

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,8 +4,9 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <script src="https://cdn.tailwindcss.com"></script>
+    {{-- <script src="https://cdn.tailwindcss.com"></script> --}}
     <title>Home Page</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
 <body class="h-full">
